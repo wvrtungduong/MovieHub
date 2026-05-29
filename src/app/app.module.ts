899972a30c25ad8b71f86c78ffde5c1d98a4a93d
wvrtungduong <wvrtungduong6102@gmail.com>
2026-05-29 15:00:00 +0700

    feat: add router and wire AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule],
+	declarations: [AppComponent, MovieListComponent, MovieCardComponent],
+	imports: [BrowserModule, AppRoutingModule],
 	bootstrap: [AppComponent]
 })
 export class AppModule {}
