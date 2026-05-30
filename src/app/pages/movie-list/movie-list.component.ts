2b62ab9c56502fe52cc14176fff1bf9c43adcf9a
wvrtungduong <wvrtungduong6102@gmail.com>
2026-05-30 10:14:52 +0700

    feat: wire movie list to movie service

--- src/app/pages/movie-list/movie-list.component.ts
+++ src/app/pages/movie-list/movie-list.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
 	selector: 'app-movie-list',
 	templateUrl: './movie-list.component.html'
 })
-export class MovieListComponent {}
+export class MovieListComponent implements OnInit {
+	movies: any[] = [];
+
+	constructor(private movieService: MovieService) {}
+
+	ngOnInit(): void {
+		this.movieService.getMovies().then((m: any[]) => {
+			this.movies = m || [];
+		}).catch(() => {
+			this.movies = [];
+		});
+	}
+}
